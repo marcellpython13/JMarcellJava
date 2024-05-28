2c18f81573fdb77f860887f8972e91aca67488c3
marcellpython13 <131479035+marcellpython13@users.noreply.github.com>
2024-05-28 11:44:46 +0200

    Update ForIterateExample.java

--- ForIterateExample.java
+++ ForIterateExample.java
@@ -12,37 +12,37 @@ public class ForIterateExample {
 		// Amikor egy vagy több utasítást többször szeretnénk ismételni.
 		// Mikor használunk FOR ciklust?
 		// Amikor tudjuk pontosan, hogy hányszor kell ismételni.
-		// Milyen részei vannak a ciklusnak? Kezdőértékadás, ciklusfeltétel, léptetés, ciklusmag
-		// Példákban kulcsszavak: ismételd 5X, tömbkezeléses műveletek, intervallum vizsgálat
-		// Adatszerkezet bejárására gyakrabban használjuk a foreach-et for ciklus helyett, ha nem kell az index
+		// Milyen részei vannak a ciklusnak? Kezdőértékadás, ciklusfeltétel, léptetés, ciklusmag.
+		// Példákban kulcsszavak: ismételd 5X, tömbkezeléses műveletek, intervallum vizsgálat.
+		// Adatszerkezet bejárására gyakrabban használjuk a foreach-et for ciklus helyett, ha nem kell az index.
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Feladat01: Névbekérés - 5X kiírás: ");
 		System.out.println("Kérem adja meg a nevét: ");
 		String fullName = br.readLine();
 		for (int i = 0; i < 5; i++) {
-			//ciklusmag
+			// ciklusmag
 			System.out.println((i+1)+".futás: "+fullName);
 		}
 		System.out.println();
 		
-		//ez is 5X fut le
+		// ez is 5X fut le
 //		for (int i = 1; i <= 5; i++) {
 //			System.out.println(i+". futás: "+fullName);
 //		}
 		
-		//ez is 5X fut le
+		// ez is 5X fut le
 //		for (int i = 1; i < 6; i++) {
 //			System.out.println(i+". futás: "+fullName);
 //		}
 		
-		//for (int i=5; i>0;i--)
-		//for (int i=0; i<10;i=i+2)
-		//for (int i=0; i<10;i+=2)
+		// for (int i=5; i>0;i--)
+		// for (int i=0; i<10;i=i+2)
+		// for (int i=0; i<10;i+=2)
 		
-		//int i = 0; kezdőértékadás
-		//i < 5 ciklusfeltétel
-		//i++ ciklusváltozó növelése 
+		// int i = 0; kezdőértékadás
+		// i < 5 ciklusfeltétel
+		// i++ ciklusváltozó növelése 
 		/*
 		 * 1. futás: i értéke 0 < 5 true - végrehajta, amit a ciklusmagban talál 
 		 * 2. futás: i értéke 1 < 5 true - végrehajta, amit a ciklusmagban talál 
@@ -58,7 +58,7 @@ public class ForIterateExample {
 		System.out.println("Feladat02: szöveg - ismétlésszám:");
 		System.out.println("Kérem adja meg a szöveget: ");
 		String text = br.readLine();
-		System.out.println("Kérem adja meg az ismétlésszámot (egész szám pld. 6): ");
+		System.out.println("Kérem adja meg az ismétlésszámot (egész szám, pl. 6): ");
 		int count = Integer.parseInt(br.readLine());
 		for (int i = 0; i < count; i++) {
 			System.out.println((i+1)+".futás: "+text);
@@ -66,7 +66,7 @@ public class ForIterateExample {
 		System.out.println();
 		
 		// Feladat03: Kérjünk be 10 db egész számot és mindegyikről külön-külön írjuk ki,
-		// hogy osztható -e hárommal maradék nélkül! Tömb nélkül oldjuk meg!
+		// hogy osztható-e hárommal maradék nélkül! Tömb nélkül oldjuk meg!
 		System.out.println("Feladat03: hárommal való m.n osztás - 10 szám:");
 		int numberInput;
 		for (int i = 0; i < 10; i++) {
@@ -76,9 +76,9 @@ public class ForIterateExample {
 				System.out.println("Hárommal m.n. osztható...");
 			} else {
 				System.out.println("Nem osztható hárommal m.n. ...");
-			} //ide ér 
+			} // ide ér 
 		}
-		//System.out.println(numberInput); //hibás, mert a numberInput-nak nincs kezdőértéke
+		// System.out.println(numberInput); // hibás, mert a numberInput-nak nincs kezdőértéke
 		System.out.println();
 		
 		// Feladat04: 10-től visszafelé 1-ig egymás mellé szóközzel 
@@ -106,15 +106,15 @@ public class ForIterateExample {
 				isAnyProperNumber = true;
 			}
 		}
-		//if (isAnyProperNumber==false) //ez is helyes
+		// if (isAnyProperNumber==false) // ez is helyes
 		if (!isAnyProperNumber) {
 			System.out.println("Sajnálom nem volt ilyen érték :(");
 		}
 		System.out.println();
 		System.out.println();
 		
-		//Feladat06: 500 és 600 között írjunk ki minden harmadik számot, egymás mellé
-		// vesszővel elválaszva, de utolsó után ne legyen vessző!
+		// Feladat06: 500 és 600 között írjunk ki minden harmadik számot egymás mellé,
+		// vesszővel elválaszva, de az utolsó után ne legyen vessző!
 		System.out.println("Feladat06: 500 és 600 között írjunk ki minden harmadik számot: ");
 		for (int i = 500; i <= 600; i=i+3) {
 			if (i>597) {
